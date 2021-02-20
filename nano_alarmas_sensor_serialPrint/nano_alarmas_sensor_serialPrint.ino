06d0f5ae98925dfdc568a1d0e0dedcf00445a1de
AngeliM96 <matiasangeli@gmail.com>
2021-02-20 04:30:42 -0300

    Calibracion y sensor utilizando serial

---
+++ nano_alarmas_sensor_serialPrint/nano_alarmas_sensor_serialPrint.ino
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------
+    Sensor de co2 usando serial
+  ----------------------------------------------------------*/
+#include <MHZ19_uart.h> // incluye librería para manejo del sensor de CO2
+
+//-----------------Pines----------------------------
+const int rx_pin = 3;     // Serial rx
+const int tx_pin = 4;     // Serial tx
+const int pinBuzzer = 12; // Buzzer
+const int pinLed = 13;    // Led
+//--------------------------------------------------
+long loops = 0; // Contamos las veces que se ejecutó el loop
+MHZ19_uart sensor; // creo el objeto del sensor
+
+//-----------------Alarma----------------------------
+void alarma(int veces, int duracionNota) {
+  for(int i=0; i<veces; i++)
+  {
+    digitalWrite(pinLed, HIGH);           // Prendo led 
+    tone(pinBuzzer, 523, duracionNota);   // Hago sonar el buzzer, 523 corresponde a la nota C5
+    delay(duracionNota);                  // Espero lo que dura la nota
+    noTone(pinBuzzer);                    // Silencio el buzzer
+    digitalWrite(pinLed , LOW);           // Apago led
+    delay(duracionNota);                  // Delay entre alarmas
+  }
+}
+
+//-----------------Setup----------------------------
+void setup() {
+  pinMode(pinBuzzer, OUTPUT); // Setteamos los pines de buzzer y led
+  pinMode(pinLed, OUTPUT);
+  Serial.begin(9600);         // Iniciamos el serial
+  alarma(1, 250);
+  Serial.print("INICIANDO");
+  delay(5000);                // Esperamos 5 segundos
+  // Calentamos 
+  sensor.begin(rx_pin, tx_pin);     // Setteamos los pines y iniciamos el sensor
+  sensor.setAutoCalibration(false); //Se setea la autocalibracion en falso para poder calibrar cuando se desee
+  Serial.print("Calentando, espere 2 minutos");
+  delay(120000);                    // Esperamos 2 minutos
+  alarma(3, 250);                    // Alarma indicando que terminó el calentamiento
+}
+
+//-----------------Loop----------------------------
+void loop() {
+  if(++loops % 30 == 0) { // Si loops es múltiplo de 30
+    alarma(1, 250); 
+    Serial.print("AireNuevo UNAHUR"); 
+    Serial.print("MEDIDOR de CO2");  
+    delay(5000); //demora 5 seg entre mediciones
+    loops = 0; // vuelve a empezar LOS LOOPS
+  } 
+  int co2ppm = sensor.getPPM(); // mide CO2
+  int temp = sensor.getTemperature(); // mide temperatura
+
+//  Muestra medición de CO2    
+  Serial.print("co2: " + String(co2ppm) + "ppm \n"); // Escribe CO2
+//  Muestra medición de temperatura
+  Serial.print("temp: " + String(temp) + "*C \n"); // Escribe temperatura
+  
+//  Emite una alarma en función del resultado
+    if(co2ppm >= 1200){
+      alarma(8, 250);
+    } 
+    else if(co2ppm >= 1000){
+      alarma(4, 500);
+    } 
+    else if(co2ppm >= 900){
+      alarma(2, 1000);
+    } 
+  delay(10000); //demora 10 seg entre mediciones
+}
---
+++ nano_calibracion_serialPrint/nano_calibracion_serialPrint.ino
@@ -0,0 +1,72 @@
+/*----------------------------------------------------------
+    Calibracion del sensor usando serial
+  ----------------------------------------------------------*/
+#include <MHZ19_uart.h>
+
+//-----------------Pines----------------------------
+const int rx_pin = 3;     // Serial rx
+const int tx_pin = 4;     // Serial tx
+const int pinBuzzer = 12; // Buzzer
+const int pinLed = 13;    // Led
+
+//--------------------------------------------------
+const long segundosEspera = 1800; // cantidad de segundos a esperar
+long segundosPasados = 0; // cuenta segundos
+MHZ19_uart sensor; // creo el objeto del sensor
+
+//-----------------Alarma----------------------------
+void alarma(int veces) {
+  for(int i=0; i<veces; i++)
+  {
+    digitalWrite(pinLed, HIGH);  // Prendo led 
+    tone(pinBuzzer, 523, 250);   // Hago sonar el buzzer, 523 corresponde a la nota C5, 250 es la duración de la nota
+    delay(250);                  // Espero lo que dura la nota
+    noTone(pinBuzzer);           // Silencio el buzzer
+    digitalWrite(pinLed , LOW);  // Apago led
+    delay(250);                  // Delay entre alarmas
+  }
+}
+
+//-----------------Setup----------------------------
+void setup() {
+  // Setteamos los pines de buzzer y led
+  pinMode(pinBuzzer, OUTPUT); 
+  pinMode(pinLed, OUTPUT);
+  sensor.begin(rx_pin, tx_pin); // Setteamos los pines y iniciamos el sensor
+  sensor.setAutoCalibration(false); //Se setea la autocalibracion en falso para poder calibrar cuando se desee
+  Serial.begin(9600); // Iniciamos el serial
+  Serial.print("Calentando \n");    // Escribe primera linea del cartel
+  Serial.print("Espere 1 minuto \n");  // Escribe segunda linea del cartel
+  delay(60000); // Espera 1 minuto
+  alarma(2);
+}
+
+//-----------------Loop-----------------------------
+void loop() {
+  if ( ++segundosPasados % 60 == 0) { // Si cnt es múltiplo de 60
+    Serial.print(String(segundosPasados / 60) + "minutos \n"); // Cada minuto muestra el valor 
+    Serial.print("co2: " + String(sensor.getPPM()) + "ppm \n"); // Escribe CO2
+    alarma(1);
+  } else {
+    Serial.print(String(segundosPasados)+ "segundos \n");    
+  }
+  
+  delay(1000); // Espera 1 segundo
+
+  if (segundosPasados > segundosEspera) { // Se pasaron 30 minutos 
+    sensor.calibrateZero(); // Calibra
+    Serial.print("PRIMERA CALIBRACION \n");    // Escribe texto
+    alarma(1);
+    delay(60000); // Espera 1 minuto
+    sensor.calibrateZero();  // Calibra por segunda vez por las dudas
+    Serial.print("SEGUNDA CALIBRACION \n");    // Escribe texto
+    alarma(2);
+    
+    for(int i=0; i<10; i++) { // muestra mediciones
+      Serial.print("co2: " + String(sensor.getPPM()) + "ppm \n");    // Escribe texto
+      delay(10000); // Espera 10 segundos
+    }
+    segundosPasados = 0; // vuelve a empezar la calibración
+    alarma(5);
+  }
+}
