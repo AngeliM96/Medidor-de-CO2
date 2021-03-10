183a68b2166f487c2310ebcc3ca5f3398a5d9bb4
AngeliM96 <matiasangeli@gmail.com>
2021-03-10 19:10:05 -0300

    Wemos funcionando con sensor, display y rgb

--- wemos_calib_sensor/wemos_calib_sensor.ino
+++ wemos_calib_sensor/wemos_calib_sensor.ino
@@ -5,11 +5,11 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 //-----------------Pines----------------------------
-const int rx_pin = 14;     // Serial rx
-const int tx_pin = 16;     // Serial tx
-const int pinLedR = 12;    // Led Rojo
-const int pinLedG = 13;    // Led Verde
-const int pinLedB = 15;    // Led Azul
+const int rx_pin = 13;     // Serial rx
+const int tx_pin = 15;     // Serial tx
+const int pinLedR = 16;    // Led Rojo
+const int pinLedG = 14;    // Led Verde
+const int pinLedB = 12;    // Led Azul
 //--------------------------------------------------
 long loops = 0;                         // Contamos las veces que se ejecutó el loop
 MHZ19_uart sensor;                      // creo el objeto del sensor
